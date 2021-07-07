99b42c6fc71ece49fab4807452676cd2407afa3b
rosinaSav <rosinasavisaar@gmail.com>
2021-07-07 13:58:31 +0200

    Add R script for installation of rstan and brms

---
+++ install_brms.r
@@ -0,0 +1,14 @@
+install.packages('tidyverse')
+
+dotR <- file.path(Sys.getenv("HOME"), ".R")
+if (!file.exists(dotR)) dir.create(dotR)
+M <- file.path(dotR, "Makevars")
+if (!file.exists(M)) file.create(M)
+cat("\nCXX14FLAGS=-O3 -march=native -mtune=native -fPIC",
+    "CXX14=g++", # or clang++ but you may need a version postfix
+    file = M, sep = "\n", append = TRUE)
+
+Sys.setenv(DOWNLOAD_STATIC_LIBV8 = 1) # only necessary for Linux without the nodejs library / headers
+install.packages("rstan", repos = "https://cloud.r-project.org/", dependencies = TRUE)
+
+install.packages("brms")
